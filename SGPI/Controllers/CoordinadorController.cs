c1fccbcf02d524f552a50e4a4b533e4c3bb1467d
Jessica Silva <Jessica Silva@LAPTOP-K77E1CI5>
2022-10-04 22:23:24 -0500

    Agregar archivos de proyecto.

---
+++ SGPI/Controllers/CoordinadorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SGPI.Controllers
+{
+    public class CoordinadorController : Controller
+    {
+        public IActionResult BuscarCoordinador()
+        {
+            return View();
+        }
+
+        public IActionResult ProgramarAsignacion()
+        {
+            return View();
+        }
+
+        public IActionResult Homologacion()
+        {
+            return View();
+        }
+
+        public IActionResult Entrevistas()
+        {
+            return View();
+        }
+
+        public IActionResult Reportes()
+        {
+            return View();
+        }
+
+    }
+}
+
---
+++ SGPI/Controllers/EstudianteController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SGPI.Controllers
+{
+    public class EstudianteController : Controller
+    {
+        public IActionResult Actualizar()
+        {
+            return View();
+        }
+
+        public IActionResult Pagos()
+        {
+            return View();
+        }
+
+    }
+}
