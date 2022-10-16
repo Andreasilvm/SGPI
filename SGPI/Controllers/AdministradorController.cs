42da7efe6bca7c5000f7c98cda96d63ed6415397
Andrea Silva <yuliandreasilvamunoz@gmail.com>
2022-10-16 00:12:05 -0500

    modificar la carpeta modal con la clase Bd

--- SGPI/Controllers/AdministradorController.cs
+++ SGPI/Controllers/AdministradorController.cs
@@ -1,11 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SGPI.Models;
 
 namespace SGPI.Controllers
 {
     public class AdministradorController : Controller
     {
+        SGPI_BDContext context = new SGPI_BDContext();
+
+
         public IActionResult Login()
         {
+
+            /// create
+
+            TblUsuario usr = new TblUsuario();
+
+            usr.PrimerNombre = "Mauricio";
+            usr.SegundoNombre = string.Empty;
+            usr.PrimerApellido = "Amariles";
+            usr.SegundoApellido = "Camacho";
+            usr.Email = "mauricio.amariles@tdea.edu.co";
+            usr.Iddoc = 1;
+            usr.Idgenero = 1;
+            usr.Idrol = 1;
+            usr.Idprograma = 1;
+            usr.NumeroDocumento = "123456789";
+            usr.VcPassword = "123456789";
+
+            context.Add(usr);
+            context.SaveChanges();
+
+            /// QUERY
+            /// UPDATE
+            /// DALATE
+
             return View();
         }
 
